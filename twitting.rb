06f658b3ad8d993a5cf6994b113486705b5d1a31
kazu <skaz0110@yahoo.co.jp>
2021-10-10 16:32:28 +0900

    標準入力からツイートできるように変更

--- twitting.rb
+++ twitting.rb
@@ -1,11 +1,17 @@
 require 'twitter'
 
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "YOUR_CONSUMER_KEY"
-  config.consumer_secret     = "YOUR_CONSUMER_SECRET"
-  config.access_token        = "YOUR_ACCESS_TOKEN"
-  config.access_token_secret = "YOUR_ACCESS_SECRET"
+  config.consumer_key        = ""
+  config.consumer_secret     = ""
+  config.access_token        = ""
+  config.access_token_secret = ""
 end
 
-client.update("Rubyからツイート @gem!")
-p "実行完了"
+p "ツイートしますか？ y/n"
+yesOrNo = gets
+if yesOrNo == "y\n"
+  twitt = gets
+  client.update(twitt)
+  p "ツイート完了"
+else  p "ツイートを中断"  
+end
